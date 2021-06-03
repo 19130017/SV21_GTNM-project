aeb028c95d8a0ec0b6e3be19b5c9f625b1ada3d7
Ân <huynhhuuan10121972@gmail.com>
2021-06-03 12:07:07 +0700

    update again contact

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -6,44 +6,33 @@ class Contact extends Component {
     return (
       <div>
         <Breadcrumb position="Contact" />
-        <div className="container-contact">
-          <h1 className="h1-contact">Contact</h1>
-          <div className="row">
-            <div className="col-sm-6">
-              <img src="img/mail.png" className="mail-img" alt="mail" />
+        <section className="login spad contact-section">
+            <div className ="container-contact">
+            <div className = "row">
+              <div className = "col-lg-6 contact-info">
+              <div><i className = "fa fa-map-marker"/>Address, Nong Lam University, HCM city</div>
+              <div><i className = "fa fa-envelope"/>hci2021group02.gmail.com</div>
+              <div>
+                <i className = "fa fa-phone" />0987609675
+              </div>
+              <div>
+                <i className = "fa fa-clock-o"/> Mon -Fri 8:00 AM to 5:00 PM
+              </div>
+              </div>
+              <div className = "col-lg-6 contact-form">
+                <h3 className = "h3-contact">Contact Us</h3>
+                <form className ="contact" method="post">
+                  <input className ="text-box" type="text" name="" value="" placeholder="Your Name" required/>
+                  <input className ="text-box" type="text" name="" value="" placeholder="Your Email" required/>
+                  <textarea row = "7" type ="text" placeholder = "Your Message" required/>
+                  <input className ="btn btn-primary" type="submit" name="" value="Send"/>
+                </form>
+              </div>
+             
             </div>
-            <div className="col-sm-6">
-              <form>
-                <div className="form-group">
-                  <label htmlFor="exampleInputPassword1">Your Name</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    placeholder="Enter your name"
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="exampleInputPassword1">Email</label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="exampleInputPassword1"
-                    placeholder="Enter your email"
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="exampleInputPassword1">Message</label>
-                  <textarea rows="4" cols="90" placeholder="Message"></textarea>
-                </div>
-                <button type="submit" className="btn btn-primary submit-btn">
-                  Submit
-                </button>
-              </form>
+            
             </div>
-          </div>
-        </div>
+        </section>
       </div>
     );
   }
